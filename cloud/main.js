ffcff96a5f9b645eaf52d52488ca9988afaf95ed
qipu <qipu_1991@126.com>
2014-11-07 13:46:48 +0800

    Debug checkEmail function 6th

--- cloud/main.js
+++ cloud/main.js
@@ -23,11 +23,9 @@ AV.Cloud.define("checkEmail", function(request, response) {
     query.equalTo("emailSuffix", request.params.emailSuffix);
     query.find({
         success:function(results) {
-            response.success(results[0].get("emailSuffix"));
             response.success("You are in TOP 500!");
         },
         error:function(error) {
-            response.error(error.code + ":" + error.message)
             response.error("You are not in TOP 500, sorry!");
         }
     }); 
