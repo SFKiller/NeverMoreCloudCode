d82d8cf85458809ebf7f361f845793c7ac53bb2a
qipu <qipu_1991@126.com>
2014-11-07 11:26:08 +0800

    Add function to check if the Email in TOP 500

--- cloud/main.js
+++ cloud/main.js
@@ -3,3 +3,29 @@
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.beforeSave("_User", function(request, response) {
+    var email = request.object.get("email");
+    var len = email.length;
+    var suffix = email.SubString(email.IndexOf('@'), len - 1);
+    AV.Cloud.run("checkEmail", {emailSuffix: 'suffix'}, {
+        success:function(data) {        
+        },
+        error:function(err) {
+            request.object.set("email", null);
+        }
+    });
+});
+
+AV.Cloud.define("checkEmail", function(request, response) {
+    var query = new AV.Query("Top500Email");
+    query.equalTo("emailSuffix", request.params.suffix);
+    query.find({
+        success:function() {
+            response.success("You are in TOP 500!");
+        },
+        error:function() {
+            response.error("You are not in TOP 500, sorry!");
+        }
+    }); 
+});
